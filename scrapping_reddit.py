4559e9a852eb358a17ebdf13b108a337902a25bb
nmarounina <nmarounina@gmail.com>
2021-09-23 14:38:26 +0200

    The program looks at all the new posts in the last 5 hours and last 3 pages of the "hot" section to get all text and CC mentions generated less than 1h ago  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   list_cc_names.dat
    	modified:   scrapping_reddit.py
    
     Changes not staged for commit:
    	modified:   images/cloud.png
    
     Untracked files:
    	.DS_Store
    	.ipynb_checkpoints/
    	get_cc_names/

--- scrapping_reddit.py
+++ scrapping_reddit.py
@@ -9,31 +9,60 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import streamlit as st
-
-#read the main page first
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-url = "https://old.reddit.com/r/CryptoCurrency/new"
-response=requests.get(url, headers=headers)
-
-soup=BeautifulSoup(response.content, "lxml")
-
-time.sleep(0.1)
-
-
-# In[2]:
-
-
 from datetime import datetime, timedelta
 
+
 now=datetime.utcnow()
-limit_time = now-timedelta(hours=5, minutes=0)
-#limit_time_post=now-timedelta(days=2, hours=0, minutes=0)
+limit_time = now-timedelta(hours=1, minutes=0)
+limit_time_post=now-timedelta(days=0, hours=5, minutes=0)
+page_limit_hot=3
+
+#first, constitute the list of pages that contain posts we want to go through
 
 
-# In[3]:
+
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
+
+url1 = "https://old.reddit.com/r/CryptoCurrency/new"
+url2 = "https://old.reddit.com/r/CryptoCurrency/"
 
 
-#Create a dictionnary to count the occurence of cryptocurrency names
+#for CryptoCurrency/new we will get ~all posts of the lats 12 hours
+post_time_obj=now
+list_soups=[]
+while  post_time_obj > limit_time_post:
+    response=requests.get(url1, headers=headers)
+    list_soups.append( BeautifulSoup(response.content, "lxml") )
+    
+    for comments_tag in list_soups[-1].find_all("a", class_="bylink comments may-blank", href=True):
+            post_all=comments_tag.parent.parent.parent
+            post_nb_of_comments=comments_tag.string
+            post_time=post_all.time["title"]
+            post_time_obj=datetime.strptime(post_time, "%a %b %d %H:%M:%S %Y %Z" )
+    
+    nav_buttons=list_soups[-1].find_all("div", class_="nav-buttons" )
+    
+    url1=nav_buttons[0].find("span", class_="next-button").a.get("href")
+    print("======",url1, post_time_obj)
+    time.sleep(0.01)
+
+
+
+#for CryptoCurrency/ = "hot" page we get 5 last pages of posts, irrespective of the time when post has been created
+for i in range(0,page_limit_hot):
+    response=requests.get(url2, headers=headers)
+    list_soups.append( BeautifulSoup(response.content, "lxml") )
+    nav_buttons=list_soups[-1].find_all("div", class_="nav-buttons" )
+    
+    url2=nav_buttons[0].find("span", class_="next-button").a.get("href")
+    print("======",url2)
+    time.sleep(0.01)
+
+
+
+
+
+#Create and initiate a dictionnary to count the occurence of cryptocurrency names
 f=open("list_cc_names.dat", "r")
 
 dict_cc={}
@@ -42,69 +71,75 @@ for name in f:
     
     if len(name)>1:
         dict_cc[name[0:-1].lower()]=0
-    
-
-
-# In[4]:
 
 
 
-#get the url of comments and creates a new soup object for the entire comment section
-
-for comments_tag in soup.find_all("a", class_="bylink comments may-blank", href=True):
-
-    url_comm = comments_tag['href']
-    post_all=comments_tag.parent.parent.parent
-    post_nb_of_comments=comments_tag.string
-
-
-    #post_time=post_all.time["title"]
-    #post_time_obj=datetime.strptime(post_time, "%a %b %d %H:%M:%S %Y %Z" )
-
-    r_comm = requests.get(url_comm, headers=headers)
-    soup_comm = BeautifulSoup(r_comm.text, "lxml")
-    print(url_comm)
-    print(post_nb_of_comments)
-    print("\n")
-    
-
-    #Get the post text
-    try :
-        post_text=soup_comm.find("div", class_="expando").getText()
-
-        for word in post_text.split():      
-            if word.lower() in dict_cc:
-                dict_cc[word.lower()]+=1
-    except:
-        print("#") #some posts have no text
 
 
 
-    #Then, let's get all of the comments:
-    comms_for_post=soup_comm.find_all("div", class_="entry unvoted")
+for soup in list_soups:
+    #get the url of comments and creates a new soup object for the entire comment section
+    for comments_tag in soup.find_all("a", class_="bylink comments may-blank", href=True):
 
-    for commt in comms_for_post:
+        url_comm = comments_tag["href"]
+        post_all=comments_tag.parent.parent.parent
+        post_nb_of_comments=comments_tag.string
 
-        try:
 
-            time_string=commt.time["title"]
-            time_obj=datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y %Z" )
+        post_time=post_all.time["title"]
+        post_time_obj=datetime.strptime(post_time, "%a %b %d %H:%M:%S %Y %Z" )
 
-            if time_obj>limit_time:
+        r_comm = requests.get(url_comm, headers=headers)
+        soup_comm = BeautifulSoup(r_comm.text, "lxml")
+        print(url_comm)
+        print(post_nb_of_comments)
+        print("\n")
+        
 
-                text=commt.find("div", class_="md").getText()
-                #print(time_obj,text)
+        #Get the post text
+        try :
+        
+            if post_time_obj>limit_time:
+                post_text=soup_comm.find("div", class_="expando").getText()
 
-                for word in text.split() :#.string.split():
-
+                for word in post_text.split():
                     if word.lower() in dict_cc:
                         dict_cc[word.lower()]+=1
-
-
         except:
-            print("*")
-    time.sleep(.01)
-    print("\n")
+            print("#") #some posts have no text
+
+
+
+        #Then, let's get all of the comments:
+        comms_for_post=soup_comm.find_all("div", class_="entry unvoted")
+
+        for commt in comms_for_post:
+
+            try:
+
+                time_string=commt.time["title"]
+                time_obj=datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y %Z" )
+
+                if time_obj>limit_time:
+
+                    text=commt.find("div", class_="md").getText()
+                    #print(time_obj,text)
+
+                    for word in text.split() :#.string.split():
+
+                        if word.lower() in dict_cc:
+                            dict_cc[word.lower()]+=1
+
+
+            except:
+                print("*")
+                
+        time.sleep(0.01)
+        print("\n")
+
+
+
+
 
 
 # In[5]:
@@ -149,7 +184,7 @@ image=Image.open("./images/cloud.png")
 st.image(image,width=700)
 
 plt.clf()
-plt.figure(figsize=(30, 10))
+plt.figure(figsize=(40, 10))
 plt.rcParams.update({"font.size": 40})
 plt.xticks(rotation=45)
 plt.bar(newdict.keys(), newdict.values(),color="brown", )
