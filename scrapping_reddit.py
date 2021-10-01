b552ce8f47c62f50f582ff178519a826e96ac58b
nmarounina <nmarounina@gmail.com>
2021-10-01 16:53:18 +0200

    Added new text to display the 10 more mentioned cc, also added a try: clode to avoind some ads  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   scrapping_reddit.py
    
     Changes not staged for commit:
    	modified:   images/cloud.png
    
     Untracked files:
    	.DS_Store
    	.ipynb_checkpoints/
    	get_cc_names/
    	streamlit_app_test.pem

--- scrapping_reddit.py
+++ scrapping_reddit.py
@@ -14,8 +14,8 @@ from datetime import datetime, timedelta
 
 now=datetime.utcnow()
 limit_time = now-timedelta(hours=1, minutes=0)
-limit_time_post=now-timedelta(days=0, hours=5, minutes=0)
-page_limit_hot=3
+limit_time_post=now-timedelta(days=0, hours=2, minutes=0)
+page_limit_hot=1
 
 #first, constitute the list of pages that contain posts we want to go through
 
@@ -24,7 +24,7 @@ page_limit_hot=3
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"}
 
 url1 = "https://old.reddit.com/r/CryptoCurrency/new"
-url2 = "https://old.reddit.com/r/CryptoCurrency/"
+#url2 = "https://old.reddit.com/r/CryptoCurrency/"
 
 
 #for CryptoCurrency/new we will get ~all posts of the lats 12 hours
@@ -48,15 +48,15 @@ while  post_time_obj > limit_time_post:
 
 
 
-#for CryptoCurrency/ = "hot" page we get 5 last pages of posts, irrespective of the time when post has been created
-for i in range(0,page_limit_hot):
-    response=requests.get(url2, headers=headers)
-    list_soups.append( BeautifulSoup(response.content, "lxml") )
-    nav_buttons=list_soups[-1].find_all("div", class_="nav-buttons" )
-    
-    url2=nav_buttons[0].find("span", class_="next-button").a.get("href")
-    print("======",url2)
-    time.sleep(0.01)
+##for CryptoCurrency/ = "hot" page we get 5 last pages of posts, irrespective of the time when post has been created
+#for i in range(0,page_limit_hot):
+#    response=requests.get(url2, headers=headers)
+#    list_soups.append( BeautifulSoup(response.content, "lxml") )
+#    nav_buttons=list_soups[-1].find_all("div", class_="nav-buttons" )
+#
+#    url2=nav_buttons[0].find("span", class_="next-button").a.get("href")
+#    print("======",url2)
+#    time.sleep(0.01)
 
 
 
@@ -80,60 +80,63 @@ for name in f:
 for soup in list_soups:
     #get the url of comments and creates a new soup object for the entire comment section
     for comments_tag in soup.find_all("a", class_="bylink comments may-blank", href=True):
-
-        url_comm = comments_tag["href"]
-        post_all=comments_tag.parent.parent.parent
-        post_nb_of_comments=comments_tag.string
-
-
-        post_time=post_all.time["title"]
-        post_time_obj=datetime.strptime(post_time, "%a %b %d %H:%M:%S %Y %Z" )
-
-        r_comm = requests.get(url_comm, headers=headers)
-        soup_comm = BeautifulSoup(r_comm.text, "lxml")
-        print(url_comm)
-        print(post_nb_of_comments)
-        print("\n")
-        
-
-        #Get the post text
-        try :
-        
-            if post_time_obj>limit_time:
-                post_text=soup_comm.find("div", class_="expando").getText()
-
-                for word in post_text.split():
-                    if word.lower() in dict_cc:
-                        dict_cc[word.lower()]+=1
-        except:
-            print("#") #some posts have no text
+        try:
+            url_comm = comments_tag["href"]
+            post_all=comments_tag.parent.parent.parent
+            post_nb_of_comments=comments_tag.string
 
 
+            post_time=post_all.time["title"]
+            post_time_obj=datetime.strptime(post_time, "%a %b %d %H:%M:%S %Y %Z" )
 
-        #Then, let's get all of the comments:
-        comms_for_post=soup_comm.find_all("div", class_="entry unvoted")
+            r_comm = requests.get(url_comm, headers=headers)
+            soup_comm = BeautifulSoup(r_comm.text, "lxml")
+            print(url_comm)
+            print(post_nb_of_comments)
+            print("\n")
+            
 
-        for commt in comms_for_post:
+            #Get the post text
+            try :
+            
+                if post_time_obj>limit_time:
+                    post_text=soup_comm.find("div", class_="expando").getText()
 
-            try:
-
-                time_string=commt.time["title"]
-                time_obj=datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y %Z" )
-
-                if time_obj>limit_time:
-
-                    text=commt.find("div", class_="md").getText()
-                    #print(time_obj,text)
-
-                    for word in text.split() :#.string.split():
-
+                    for word in post_text.split():
                         if word.lower() in dict_cc:
                             dict_cc[word.lower()]+=1
-
-
             except:
-                print("*")
-                
+                print("#") #some posts have no text
+
+
+
+            #Then, let's get all of the comments:
+            comms_for_post=soup_comm.find_all("div", class_="entry unvoted")
+
+            for commt in comms_for_post:
+
+                try:
+
+                    time_string=commt.time["title"]
+                    time_obj=datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y %Z" )
+
+                    if time_obj>limit_time:
+
+                        text=commt.find("div", class_="md").getText()
+                        #print(time_obj,text)
+
+                        for word in text.split() :#.string.split():
+
+                            if word.lower() in dict_cc:
+                                dict_cc[word.lower()]+=1
+
+
+                except:
+                    print("*")
+        except:
+            print("&")
+            
+            
         time.sleep(0.01)
         print("\n")
 
@@ -154,7 +157,30 @@ for entry in dict_cc:
         newdict[entry]=dict_cc[entry]
 
 
+
+
+
+
 # In[6]:
+
+
+
+
+
+# Display stuff >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#let's get the ten most popular notions and then display them
+list1=sorted(dict_cc.items(), reverse=True, key=lambda item: item[1])
+
+
+st.text("Ten most popular mentions in the last hour:")
+i=1
+for k,v in list1:
+    string_to_print= "#"+str(i)+" "+ str(k)+" with "+str(v)+" mentions"
+    st.text( string_to_print )
+    i+=1
+    if i==11 :
+        break
+
 
 
 from PIL import Image
@@ -166,14 +192,11 @@ from wordcloud import WordCloud
 #"copper"
 #BrBG
 #PuOr
-#twilight (little confusing tho)
+#twilight
 wc = WordCloud(background_color="white",width=1000,height=1000, colormap="copper",
-               relative_scaling=0.5,normalize_plurals=False, font_path='SFCompactRounded').generate_from_frequencies(dict_cc)
+               relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(dict_cc)
 plt.imshow(wc)
 plt.axis("off")
-
-
-# In[9]:
 
 
 # In[9]:
